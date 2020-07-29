a4da0fe06dcee1ae38886c5053aecf4efc2b1bbf
Davy McAleer <dmcaleer@chef.io>
2020-07-29 12:06:11 +0100

    updated version to 2.10.2 and changed to .gz
    Signed-off-by: Davy McAleer <dmcaleer@chef.io>

--- plan.sh
+++ plan.sh
@@ -1,13 +1,13 @@
 pkg_name=freetype
-pkg_version=2.9.1
+pkg_version=2.10.2
 pkg_origin=core
 pkg_description="A software library to render fonts"
 pkg_upstream_url="https://www.freetype.org"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('FreeType' 'GPL-2.0')
-pkg_source=http://download.savannah.gnu.org/releases/freetype/${pkg_name}-${pkg_version}.tar.bz2
+pkg_source=http://download.savannah.gnu.org/releases/freetype/${pkg_name}-${pkg_version}.tar.gz
 pkg_filename=${pkg_name}-${pkg_version}.tar.gz
-pkg_shasum=db8d87ea720ea9d5edc5388fc7a0497bb11ba9fe972245e0f7f4c7e8b1e1e84d
+pkg_shasum="e09aa914e4f7a5d723ac381420949c55c0b90b15744adce5d1406046022186ab"
 pkg_build_deps=(
   core/diffutils
   core/gcc
